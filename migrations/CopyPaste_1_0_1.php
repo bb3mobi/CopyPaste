d79de57f59e35da929b6e3c30f2d23ba1002e4e4
saturnz <saturnz@096.net.ua>
2015-12-16 21:01:13 +0500

    Initial commit

---
+++ migrations/CopyPaste_1_0_1.php
@@ -0,0 +1,42 @@
+<?php
+/**
+*
+* @package phpBB Extension - CopyPaste
+* @copyright (c) 2015 saturn-z
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+namespace saturnZ\CopyPaste\migrations;
+
+class CopyPaste_1_0_1 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return;
+	}
+
+	static public function depends_on()
+	{
+		return array('\phpbb\db\migration\data\v310\dev');
+	}
+
+	public function update_schema()
+	{
+		return array(
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+		);
+	}
+
+	public function update_data()
+	{
+		return array(
+			// Current version
+			array('config.add', array('CopyPaste_version', '1.0.1')),
+		);
+	}
+}
