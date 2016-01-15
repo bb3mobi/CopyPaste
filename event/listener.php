668eddf713e5517ed487bc8ffe696cf05714dc61
saturnz <saturnz@096.net.ua>
2016-01-15 19:15:30 +0500

    Instal commit

--- event/listener.php
+++ event/listener.php
@@ -25,8 +25,19 @@ class listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
+            'core.user_setup'                        => 'load_language_on_setup',
 		);
 	}
+
+    public function load_language_on_setup($event)
+    {
+        $lang_set_ext = $event['lang_set_ext'];
+        $lang_set_ext[] = array(
+            'ext_name' => 'saturnZ/CopyPaste',
+            'lang_set' => 'copypaste_lng',
+        );
+        $event['lang_set_ext'] = $lang_set_ext;
+    }
 
 	/** @var \phpbb	emplate	emplate */
 	protected $template;
---
+++ language/en/copypaste_lng.php
@@ -0,0 +1,22 @@
+<?php
+/**
+*
+* @package phpBB Extension - CopyPaste
+* @copyright (c) 2015 saturn-z
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'COPYRIGHT_NAME'		=> '© Source',
+));
